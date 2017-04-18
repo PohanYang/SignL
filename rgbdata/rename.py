f26e5d330396ba6c0ffa6a8758ce5696c2f342ce
PohanYang <swingcowrock@gmail.com>
2017-04-18 11:48:48 +0800

    0418 begin

--- rgbdata/rename.py
+++
@@ -1,8 +0,0 @@
-import os
-import glob
-count = 0
-path = '5'
-for filename in glob.glob(os.path.join(path, '*.png')):
-        newname = 'training/5_'+str(count)+'.png'
-        count = count + 1
-        os.rename(filename,newname)
